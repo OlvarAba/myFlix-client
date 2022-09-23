99fa3b07c3817ae3b8faa43fcc33aa657c16ef8f
Olvar Abadeen <oabadeen@gmail.com>
2022-09-23 14:27:22 -0700

    Fixed Registration View

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import {Form, Button, Card, CardGroup, Container, Col, Row} from 'react-bootstrap';
-
+import PropTypes from "prop-types";
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 import './registration-view.scss';
 
 export function RegistrationView(props) {
@@ -10,67 +9,106 @@ export function RegistrationView(props) {
     const [ password, setPassword ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ birthday, setBirthday ] = useState('');
+    const [ values, setValues] = useState({
+      nameErr: '',
+      usernameErr: '',
+      passwordErr: '',
+      emailErr: '',
+    });
+
+    const validate = () => {
+      let isReq = true;
+      if (!username) {
+        setValues({ values, usernameErr: "Username is required" });
+        isReq = false;  
+      } else if (username.length < 5) {
+        setValues({
+          values,
+          usernameErr: "Username must be 5 characters long.",
+        });
+        isReq = false;
+      }
+      if (!password) {
+        setValues({ values, passwordErr: "Password Required" });
+        isReq = false;
+      } else if (password.length < 6) {
+        setValues({
+          values,
+          passwordErr: "Password must be 6 characters long",
+        });
+      }
+      if (!email) {
+        setValues({ values, emailErr: "Email Required" });
+        isReq = false;
+      } else if (email.indexOf("@") === -1) {
+        setValues({ values, emailErr: "Email is invalid" });
+        isReq = false;
+      }
+      return isReq;
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-       props.onRegistration(username)
-    };
-
-    axios.post('https://mantiflix.herokuapp.com/users', {
+       const isReq = validate();
+       if (isReq) {
+        axios.post('https://mantiflix.herokuapp.com/users', {
+      Name: name,
       Username: username,
       Password: password,
       Email: email,
-      Birthday: birthday
+      Birthday: birthday,
     })
     .then(response => {
       const data = response.data;
       console.log(data);
+      alert("Registration successful, please login");
       window.open('/', '_self'); 
     })
-    .catch(e => {
-      console.log('error registering the user')
+    .catch((response) => {
+      console.error(response);
+      alert("unable to register");
     });
+       }
+    };
+
+
 
     return (
-      <Container>
-        <Row>
-          <Col>
-          <CardGroup>
-            <Card>
-              <Card.Title>Register Here!</Card.Title>
-            <Form>
-        <Form.Group>
+        <Row className='mt=5'>
+          <Col md={12}>
+          <Form>
+            <h3>Register Here</h3>
+        <Form.Group controlId='formUsername' className='reg-inputs'>
           <Form.Label>Username:</Form.Label>
           <Form.Control
             type="text"
             value={username}
+            placeholder="Enter a Username"
             onChange={(e) => setUsername(e.target.value)}
-            required
-            placeholder="Enter a username"
           />
+          {values.usernameErr && <p>{values.usernameErr}</p>}
         </Form.Group>
   
-        <Form.Group>
+        <Form.Group controlId='formPassword' className='reg-inputs'>
           <Form.Label>Password:</Form.Label>
           <Form.Control
             type="password"
             value={password}
+            placeholder="Enter a Password"
             onChange={(e) => setPassword(e.target.value)}
-            required
-            minLength={8}
-            placeholder="Password must be 8 characters long"
           />
+          {values.passwordErr && <p>{values.passwordErr}</p>}
         </Form.Group>
   
-        <Form.Group>
+        <Form.Group controlId='Email' className='reg-inputs'>
           <Form.Label>Email:</Form.Label>
           <Form.Control
             type="email"
             value={email}
-            required
+            placeholder="Enter a Valid Email"
             onChange={(e) => setEmail(e.target.value)}
           />
-          
+          {values.emailErr && <p>{values.emailErr}</p>}        
         </Form.Group>
   
         <Form.Group>
@@ -81,24 +119,24 @@ export function RegistrationView(props) {
             onChange={(e) => setBirthday(e.target.value)}
           />
         </Form.Group>
-  
-        <Button variant="primary" type="submit" onClick={handleSubmit}>Submit
-        </Button>
+        <Button className='register' variant='primary' type='submit' onClick={handleSubmit}> Register</Button>
+        <p></p>
+        <p>
+            Already registered? <Link to={"/"}>Sign in</Link>
+          </p>  
       </Form>
-
-            </Card>
-          
-
-          </CardGroup>
-          
           </Col>
         </Row>
-      </Container>
-      
     );
   }
   
 
-RegistrationView.propTypes = {
-    onRegistration: PropTypes.func.isRequired,
-};
+  RegistrationView.propTypes = {
+    register: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Username: PropTypes.string.isRequired,
+      Password: PropTypes.string.isRequired,
+      Email: PropTypes.string.isRequired,
+      Birthday: PropTypes.string.isRequired,
+    }),
+  };
